6e553f97bccb20ef8ead15c65e8e21d4d5fa7da9
Valentin Capitaine <valentin.capitaine@outlook.fr>
2017-01-20 23:43:48 +0100

    Menu v.0.1

---
+++ Global2017/Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject MenuGroup;
+	public GameObject NewGameGroup;
+	public GameObject CreditsGroup;
+
+	public void NewGame() {
+		MenuGroup.SetActive(false);
+		NewGameGroup.SetActive(true);
+	}
+
+	public void Credits() {
+		MenuGroup.SetActive(false);
+		CreditsGroup.SetActive(true);
+	}
+
+	public void Back() {
+		NewGameGroup.SetActive(false);
+		CreditsGroup.SetActive(false);
+		MenuGroup.SetActive(true);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
