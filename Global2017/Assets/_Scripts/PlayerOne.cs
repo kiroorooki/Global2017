a32945f38b8322cae1d76b35a8d0bf92faf392eb
Valentin Capitaine <valentin.capitaine@outlook.fr>
2017-01-20 21:03:51 +0100

    Système de déplacement J1 & J2

---
+++ Global2017/Assets/_Scripts/PlayerOne.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOne : MonoBehaviour {
+
+	private float playerOneSpeed;
+	public float normalSpeed;
+	public float sneakySpeed;
+
+	PlayerController controller;
+
+
+	void Start () {
+		playerOneSpeed = normalSpeed;
+	}
+
+	void Awake() {
+		controller = GetComponent<PlayerController> ();
+	}
+
+	void Update () {
+		// Déplacement Normal
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("HorizontalP1"), 0, Input.GetAxisRaw ("VerticalP1"));
+		Vector3 moveVelocity = moveInput.normalized * playerOneSpeed;
+		controller.Move (moveVelocity);
+
+		// Déplacement Sneaky
+		if (Input.GetButtonDown ("Slow")) {
+			playerOneSpeed = sneakySpeed;
+		}
+		if (Input.GetButtonUp ("Slow")) {
+			playerOneSpeed = normalSpeed;
+		}
+
+	}
+
+}
---
+++ Global2017/Assets/_Scripts/PlayerTwo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTwo : MonoBehaviour {
+
+	private float playerTwoSpeed;
+	public float normalSpeed;
+	public float sneakySpeed;
+
+	PlayerController controller;
+
+
+	void Start () {
+		playerTwoSpeed = normalSpeed;
+	}
+
+	void Awake() {
+		controller = GetComponent<PlayerController> ();
+	}
+
+	void Update () {
+		// Déplacement Normal
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("HorizontalP2"), 0, Input.GetAxisRaw ("VerticalP2"));
+		Vector3 moveVelocity = moveInput.normalized * playerTwoSpeed;
+		controller.Move (moveVelocity);
+
+		// Déplacement Sneaky
+		if (Input.GetButtonDown ("Slow")) {
+			playerTwoSpeed = sneakySpeed;
+		}
+		if (Input.GetButtonUp ("Slow")) {
+			playerTwoSpeed = normalSpeed;
+		}
+
+	}
+
+}
